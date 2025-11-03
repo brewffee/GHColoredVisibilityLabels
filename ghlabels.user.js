4b66e15eddb496562440f02db59a97c5fd9d1214
bree ᓚᘏᗢ <68980781+brewffee@users.noreply.github.com>
2025-11-02 20:45:11 -0500

    Move label styles to a configuragle object

--- ghlabels.user.js
+++ ghlabels.user.js
@@ -10,38 +10,32 @@
 (function() {
     'use strict';
 
-    function applyStyles() {
-        // Private, Public, and Public template labels
-        document.querySelectorAll('span.Label.Label--secondary').forEach(l => {
-            switch (l.innerText) {
-                case "Public":
-                    l.style.background = 'linear-gradient(#55c675, #1e883c)';
-                    l.style.color = "white";
-                    l.style.border = "2px solid #1e883c";
-                    break;
-                case "Public template":
-                    l.style.background = "linear-gradient(#74adf0, #0969da)";
-                    l.style.color = "white";
-                    l.style.border = "2px solid #0969da";
-                    break;
-                case "Private":
-                    l.style.background = "linear-gradient(#C468F9 , #8F2EC6)";
-                    l.style.color = "white";
-                    l.style.border = "2px solid #8F2EC6";
-                    break;
-            }
-        });
+    const DEFAULT_STYLES = {
+        'Public': {
+            background: 'linear-gradient(#55c675, #1e883c)',
+            color: 'white',
+            border: '2px solid #1e883c',
+        },
+        'Public template': {
+            background: 'linear-gradient(#74adf0, #0969da)',
+            color: 'white',
+            border: '2px solid #0969da',
+        },
+        'Private': {
+            background: 'linear-gradient(#C468F9 , #8F2EC6)',
+            color: 'white',
+            border: '2px solid #8F2EC6',
+        },
+        'Public archive': {
+            background: 'linear-gradient(#eaac32, #9a6700)',
+            color: 'white',
+            border: '2px solid #9a6700',
+        }
+    }
 
-        // Public archive
-        // This one is already visible enough on its own, but it's nice to match :3
-        document.querySelectorAll('span.Label.Label--attention').forEach(l => {
-            switch (l.innerText) {
-                case "Public archive":
-                    l.style.background = "linear-gradient(#eaac32, #9a6700)";
-                    l.style.color = "white";
-                    l.style.border = "2px solid #9a6700";
-                    break;
-            }
+    function applyStyles() {
+        document.querySelectorAll('span.Label.Label--secondary, span.Label.Label--attention').forEach(l => {
+            if (DEFAULT_STYLES[l.innerText]) Object.assign(l.style, DEFAULT_STYLES[l.innerText]);
         });
     }
 
